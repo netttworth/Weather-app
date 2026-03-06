1b28aa7a189ba6243b3ccf6b6f2222a888ded841
netttworth <ayden.minarelli@gmail.com>
2026-03-06 12:39:31 +0900

    weather_app.py

--- weather app.py
+++ weather app.py
@@ -1,3 +1,4 @@
+# Weather App - Enter a city name to retrieve current weather conditions, temperature, and a description using the OpenWeatherMap API.
 import sys
 import requests
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
@@ -166,4 +167,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     weather_app = WeatherApp()
     weather_app.show()
+
     sys.exit(app.exec_())
